3cf5e672b00a1e5f58a0992bf854f029294c7ec7
elvismaicol <elvismaicol@gmail.com>
2020-01-05 21:31:37 -0300

    Fim

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
 
-/**
- *
- * @author Elvis
- */
+import entities.Product;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import services.CalculationService;
+
 public class Program {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        List<Product> list = new ArrayList<>();
+        
+        String path = "C:\\temp\\in.txt";
+        
+        try (BufferedReader br = new BufferedReader (new FileReader(path))) {
+            
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+            
+            Product x = CalculationService.max(list);
+            System.out.println("Most expensive: ");
+            System.out.println(x);
+                    
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
-    
 }
--- src/services/CalculationService.java
+++ src/services/CalculationService.java
@@ -3,13 +3,13 @@ package services;
 import java.util.List;
 
 public class CalculationService {
-    public static Integer max(List<Integer> list) {
+    public static < T extends Comparable<T>> T max(List<T> list) {
         if (list.isEmpty()) {
             throw new IllegalStateException("List can't be empty");
         }
-        Integer max = list.get(0);
-        for (Integer item : list) {
-            if (item.compateTo(max) > 0) {
+        T max = list.get(0);
+        for (T item : list) {
+            if (item.compareTo(max) > 0) {
                 max = item;
             }
         }
